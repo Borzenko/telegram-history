146507f0c745b205012d3864f2aa3596fc9372f2
holodnyakdasha <holodnyakdasha@gmail.com>
2019-12-27 17:18:13 +0200

    fixed adding channels to table

--- channelsMethods.js
+++ channelsMethods.js
@@ -20,6 +20,7 @@ const getChannelData = async (id) => {
     const isChannelInfoUpdated = result && oldChannelData ? isDataUpdated(result, oldChannelData) : false
     if(!checkChannelInDB && !oldChannelData && !isChannelInfoUpdated) {
         await updateChannelInfoInDB(id, dbObj, result)
+        return result
     }
     if(!isChannelInfoUpdated) {
         if(result.title !== oldChannelData.title) {
@@ -41,6 +42,7 @@ const getChannelData = async (id) => {
             dbObj.lastUpdatedAvatar = checkChannelInDB.lastUpdatedAvatar
         }
         await updateChannelInfoInDB(id, dbObj, result)
+        return result
 
     } else {
         dbObj.lastUpdatedDescription = checkChannelInDB.lastUpdatedDescription
@@ -71,7 +73,6 @@ const updateChannelInfoInDB = async (id, dbObj, requestResult) => {
             'lastUpdatedAvatar': dbObj.lastUpdatedAvatar
         }
         }, { "upsert": true })
-        return requestResult
 }
 
 const joinChannel = async(channel) => {
