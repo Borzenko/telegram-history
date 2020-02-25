4951a7b7ed6d7e4caf75a6db7def33411c303a6a
Borzenko Mihail <borzenkomihail@yahoo.com>
2020-02-25 14:54:12 +0200

    Added basic query to the cron job

--- server/cron.js
+++ server/cron.js
@@ -1,16 +1,12 @@
-// const cron = require('node-cron')
-// const db = require('./db').collection('channels')
-// const {checkDate, getChannelData} = require('./channelsMethods')
-// cron.schedule('00 44 * * * *', async () => {
-//     const channels = await (await db).find({}).toArray()
-//     console.log(channels)
-//     // const filteredArray = channels.map(item => {
-//     //     if(checkDate(item.updateTime)) {
-//     //         return item
-//     //     }
-//     // })
-//     channels.forEach(async item => {
-//         console.log(item)
-//        return await getChannelData(parseInt(item.channel_id))
-//     })
-// })
+const cron = require('node-cron')
+const db = require('./db').collection('channels')
+
+const { checkDate, getChannelData} = require('./channelsMethods')
+// run cron every 5 minutes
+cron.schedule('*/5 * * * *', async () => {
+    const channels = await (await db).find({}).sort({updateTime: -1}).limit(10).toArray()
+
+    channels.forEach(async item => {
+       return await getChannelData(parseInt(item.channel_id))
+    })
+})
